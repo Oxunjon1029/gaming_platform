a026065d7129c87115f737f58db7906e4d7d2eaf
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-06-11 00:47:54 +0500

    changed url

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,7 @@ import { io } from 'socket.io-client';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const socket = io(
-  'https://railway.app/project/4c13d7e7-4749-44be-bb15-3842359f08c8/service/041af19c-8248-4eeb-ab8a-5f022de4ac3b?id=76cfbe00-dfdc-4f90-93ac-cc4cec00a51b&',
-  {
-    extraHeaders: {
-      'Access-Control-Allow-Origin':
-        'https://railway.app/project/4c13d7e7-4749-44be-bb15-3842359f08c8/service/041af19c-8248-4eeb-ab8a-5f022de4ac3b?id=76cfbe00-dfdc-4f90-93ac-cc4cec00a51b&',
-    },
-  }
+  'https://railway.app/project/4c13d7e7-4749-44be-bb15-3842359f08c8/service/041af19c-8248-4eeb-ab8a-5f022de4ac3b?id=76cfbe00-dfdc-4f90-93ac-cc4cec00a51b&'
 );
 function App() {
   return (
